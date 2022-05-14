9e0bba76487548f027092bd50feaca71a7f4a3a0
tjansen <tjansen@enrise.com>
2022-05-14 11:49:15 +0200

    * add pretty/proper xpath * add todos * remove old code todo: * highlight new elements * mark locations of missing elements * mark moved elements * mark locations of old location * add option ignore (regex of xpath to ignore) * add option maxdiff (max difference in px) * add option diffStyle (record and diff the calculated styles as well) * add option diffHoverStyle (record and diff the calculated styles as well) * if name is 'undefined' then use the current test title * use the current spec file-name as subfolder in ${snapshotsDir} * add command to crawl the current page

--- src/command.js
+++ src/command.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-restricted-syntax */
-const fs = require('fs');
 
 Object.defineProperty(exports, '__esModule', {
   value: true,
@@ -40,18 +39,45 @@ const failOnSnapshotDiff =
   typeof Cypress.env('failOnSnapshotDiff') === 'undefined';
 const snapshotsDir = `${screenshotsFolder}/../snapshots`;
 
-function getPathTo(element) {
-  if (element.id !== '') return `id("${element.id}")`;
-  if (element === document.body) return element.tagName;
-
-  let ix = 0;
-  const siblings = element.parentNode.childNodes;
-  for (let i = 0; i < siblings.length; i++) {
-    const sibling = siblings[i];
-    if (sibling === element)
-      return `${getPathTo(element.parentNode)}/${element.tagName}[${ix + 1}]`;
-    if (sibling.nodeType === 1 && sibling.tagName === element.tagName) ix += 1;
+function getXPath(node) {
+  if (node === document.body) {
+    return `//${element.tagName}`;
   }
+  if (typeof node.hasAttribute === 'undefined') {
+    return '/';
+  }
+
+  let old = '';
+  if (node.hasAttribute('id')) {
+    old = `/${node.tagName}[@id='${node.id}']`;
+  } else if (node.hasAttribute('class')) {
+    old = `/${node.tagName}[@class='${node.getAttribute('class')}']`;
+  } else {
+    //get the sibling #
+    const siblings = node.parentNode.childNodes;
+    let sibIndex = -1;
+    for (let i = 0; i < siblings.length; i++) {
+      const sibling = siblings[i];
+      if (sibling !== node && sibling.tagName === node.tagName) {
+        sibIndex = i + 1;
+        break;
+      }
+    }
+    if (sibIndex > 0) {
+      old = `/${node.tagName}[${sibIndex}]`;
+    } else {
+      old = '/' + node.tagName;
+    }
+  }
+
+  let new_path;
+  if (node.parentNode.tagName === 'HTML') {
+    new_path = '//HTML' + old;
+  } else {
+    new_path = getXPath(node.parentNode) + old;
+  }
+
+  return new_path;
 }
 
 function saveAsJson(shotFN, geoMap) {
@@ -67,68 +93,106 @@ function diffKeys(oldKeys, newKeys) {
   };
 }
 
+function removeLastInstance(removeString, subjectStr) {
+  var charpos = subjectStr.lastIndexOf(removeString);
+  if (charpos < 0) return subjectStr;
+  let ptone = subjectStr.substring(0, charpos);
+  let pttwo = subjectStr.substring(charpos + removeString.length);
+  return ptone + pttwo;
+}
+
 function checkForNewElements(keysDiff) {
   let shouldFail = false;
+  let newElements = 0;
+  let failLines = '';
   if (keysDiff.added.length > 0) {
     shouldFail = true;
     for (const key of keysDiff.added) {
+      failLines += `New element: ${key}\n`;
       cy.log(`New element: ${key}`);
+      newElements++;
     }
   }
-  return shouldFail;
+  if (newElements > 0) {
+    cy.log(`${newElements} new elements found`);
+    failLines = `${newElements} new elements found:\n  - ${failLines.replaceAll(
+      '\n',
+      '\n  - '
+    )}`;
+    failLines = removeLastInstance('  - ', failLines);
+  }
+  return failLines;
 }
 
 function checkForMissingElements(keysDiff) {
-  let shouldFail = false;
+  let missingElements = 0;
+  let failLines = '';
+
   if (keysDiff.removed.length > 0) {
-    shouldFail = true;
-    for (const key of keysDiff.added) {
-      cy.log(`Removed element: ${key}`);
+    for (const key of keysDiff.removed) {
+      failLines += `Removed element: ${key}\n`;
+      missingElements++;
     }
   }
-  return shouldFail;
+  if (missingElements > 0) {
+    failLines = `${missingElements} missing elements:\n  - ${failLines.replaceAll(
+      '\n',
+      '\n  - '
+    )}`;
+    failLines = removeLastInstance('  - ', failLines);
+  }
+  return failLines;
 }
 
 function checkBounds(keysDiff, geoMapExisting, geoMap, shouldFail) {
+  let failLines = '';
+
   for (const key of keysDiff.common) {
     const oldBounds = geoMapExisting[key];
     const newBounds = geoMap[key];
     let boundsDiffer = false;
+    let differkeys = '';
     for (const boundsKey of Object.keys(oldBounds)) {
       if (oldBounds[boundsKey] !== newBounds[boundsKey]) {
         boundsDiffer = true;
         shouldFail = true;
-        break;
+        differkeys += ' ' + boundsKey;
       }
     }
     if (boundsDiffer) {
-      cy.log(
-        `Element bounds differ:${JSON.stringify({
-          saved: oldBounds,
-          observed: newBounds,
-        })}`
-      );
+      differkeys = differkeys.trim();
+      differkeys = differkeys.replaceAll(' ', ', ');
+      failLines += `- ${differkeys}:\n    ${JSON.stringify(
+        { path: key, saved: oldBounds, observed: newBounds },
+        null,
+        2
+      ).replaceAll('\n', '\n    ')}\n`;
     }
   }
-  return shouldFail;
+  if (failLines.length > 0) {
+    failLines = `Bounds differ:\n ${failLines.replaceAll('\n', '\n  ')}`;
+  }
+  return failLines;
 }
 
 function compareSavedAndCurrentBounds(geoMapExisting, geoMap) {
   let shouldFail = false;
+  let failLines = '';
   const keysDiff = diffKeys(Object.keys(geoMapExisting), Object.keys(geoMap));
   // new element
-  shouldFail = shouldFail || checkForNewElements(keysDiff);
+  failLines += checkForNewElements(keysDiff);
+  //todo: highlight new elements
 
   // missing element
-  shouldFail = shouldFail || checkForMissingElements(keysDiff);
+  failLines += checkForMissingElements(keysDiff);
+  //todo: mark locations of missing elements
 
   // Bounds
-  return shouldFail || checkBounds(keysDiff, geoMapExisting, geoMap);
-}
+  failLines += checkBounds(keysDiff, geoMapExisting, geoMap);
+  //todo: mark moved elements
+  //todo: mark locations of old location
 
-function loadAsJson(snapFN) {
-  const geoMapExisting = JSON.parse(fs.readFileSync(snapFN));
-  return geoMapExisting;
+  return failLines;
 }
 
 function MatchGeometrySnapshotCommand(defaultOptions) {
@@ -139,14 +203,16 @@ function MatchGeometrySnapshotCommand(defaultOptions) {
       (typeof maybeName === 'string' ? commandOptions : maybeName) || {}
     );
 
-    cy.task(_constants.MATCH, {
-      screenshotsFolder,
-      updateSnapshots,
-      options,
-    });
+    //todo: add option ignore (regex of xpath to ignore)
+    //todo: add option maxdiff (max difference in px)
+    //todo: add option diffStyle (record and diff the calculated styles as well)
+    //todo: add option diffHoverStyle (record and diff the calculated styles as well)
 
     const name = typeof maybeName === 'string' ? maybeName : undefined;
+    //todo: if name is 'undefined' then use the current test title
+
     const target = subject ? cy.wrap(subject) : cy;
+    cy.log(`Name is '${name}'`);
     // target.screenshot(name, options);
     cy.log('Get the bounds of all visible elements');
     cy.get(':visible').each((visibleElement, elementIndex, allElements) => {
@@ -156,7 +222,7 @@ function MatchGeometrySnapshotCommand(defaultOptions) {
         for (let i = 0; i < allElements.length; i += 1) {
           const element = allElements[i];
           const bounds = element.getBoundingClientRect();
-          const elemXpath = getPathTo(element);
+          const elemXpath = getXPath(element);
           allBounds[elemXpath] = bounds;
         }
         // wrap allBounds and make into an alias to get the value out of this "each" async block
@@ -168,48 +234,29 @@ function MatchGeometrySnapshotCommand(defaultOptions) {
       saveAsJson(shotFN, geoMap);
 
       const snapFN = `${snapshotsDir}/${name}.json`;
+      //todo: use the current spec file-name as subfolder in ${snapshotsDir}
+
       cy.log(`Looking for ${snapFN}`);
       cy.task('fileExists', { path: snapFN }).then(itExists => {
         if (itExists) {
           cy.log('Existing version found in snapshots');
-          const geoMapExisting = loadAsJson(snapFN);
-          cy.log('Compare current and saved bounds');
-          const shouldFail = compareSavedAndCurrentBounds(
-            geoMapExisting,
-            geoMap
-          );
-
-          if (!shouldFail) {
-            cy.log('No differences found');
-          } else {
-            throw 'Differences were found';
-          }
+          cy.task('loadAsJson', { path: snapFN }).then(geoMapExisting => {
+            cy.log('Compare current and saved bounds');
+            const shouldFail = compareSavedAndCurrentBounds(
+              geoMapExisting,
+              geoMap
+            );
+            if (shouldFail) {
+              throw 'Differences were found\n' + shouldFail;
+            } else {
+              cy.log('No differences found');
+            }
+          });
         } else {
           saveAsJson(snapFN, geoMap);
         }
       });
     });
-
-    /*        return cy.task(_constants.RECORD).then(({
-                                                            pass,
-                                                            added,
-                                                            updated,
-                                                            diffSize,
-                                                            imageDimensions,
-                                                            diffRatio,
-                                                            diffPixelCount,
-                                                            diffOutputPath
-                                                        }) => {
-                    if (!pass && !added && !updated) {
-                        const message = diffSize ? `Image size (${imageDimensions.baselineWidth}x${imageDimensions.baselineHeight}) different than saved snapshot size (${imageDimensions.receivedWidth}x${imageDimensions.receivedHeight}).\nSee diff for details: ${diffOutputPath}` : `Image was ${diffRatio * 100}% not the same as saved snapshot with ${diffPixelCount} different pixels.\nSee diff for details: ${diffOutputPath}`;
-
-                        if (failOnSnapshotDiff) {
-                            throw new Error(message);
-                        } else {
-                            Cypress.log({message});
-                        }
-                    }
-                }); */
   };
 }
 
@@ -227,4 +274,5 @@ function addMatchGeometrySnapshotCommand(
     },
     MatchGeometrySnapshotCommand(options)
   );
+  //todo: add command to crawl the current page
 }
